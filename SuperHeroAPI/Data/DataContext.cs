32a22d8f26f86dd0df35f1b9be523fac354d8261
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2022-12-10 13:31:27 -0300

    add entity framework and create data context

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=desktop-2n6p5us\\mssqlserver01;Initial Catalog=superherodb;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
