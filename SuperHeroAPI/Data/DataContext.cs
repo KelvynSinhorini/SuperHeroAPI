d010d6c72ae5d185cf5e60a2c389586c2c4f9d2c
Kelvyn Sinhorini <kelvyn.schadeck@audaztec.com.br>
2023-06-18 17:00:31 -0300

    [FEAT] Nova conexão do banco de dados

--- SuperHeroAPI/Data/DataContext.cs
+++ SuperHeroAPI/Data/DataContext.cs
@@ -4,12 +4,15 @@ namespace SuperHeroAPI.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=desktop-2n6p5us\\mssqlserver01;Initial Catalog=superherodb;Integrated Security=True;TrustServerCertificate=True");
+            var connectionString = "Server=tcp:skstudying.database.windows.net,1433;Initial Catalog=estudos;Persist Security Info=False;User ID=developer;Password=dev@0033;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";//_configuration["ConnectionStrings:databaseConnection"];
+            optionsBuilder.UseSqlServer(connectionString);
         }
 
         public DbSet<SuperHero> SuperHeroes { get; set; }
